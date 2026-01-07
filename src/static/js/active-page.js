56fa612e3b53fd6132850d155bb2698e20a3832e
Brandon Kelley <brandon@kelley-family.org>
2026-01-06 19:36:45 -0800

    Fix mobile scroll bug.

--- src/static/js/active-page.js
+++ src/static/js/active-page.js
@@ -2,11 +2,9 @@ function setActivePage() {
     const currentPath = window.location.pathname;
     const navLinks = document.querySelectorAll('.elcom-links a');
 
-    console.log(currentPath);
-
     navLinks.forEach(link => {
         const linkPath = link.getAttribute('href');
-        console.log(linkPath);
+
         if (linkPath == currentPath) {
             link.classList.add('elcom-active');
         } else {
