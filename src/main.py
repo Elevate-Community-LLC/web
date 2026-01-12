fe3c6dfed5167849a214fbdc1713e63ac4d097e2
Brandon Kelley <brandon@kelley-family.org>
2026-01-12 10:51:31 -0800

    Add platforms section.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,22 @@
 """Serve static files in this directory."""
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+class NoCacheStaticFiles(StaticFiles):
+    def file_response(self, *args, **kwargs) -> Response:
+        response = super().file_response(*args, **kwargs)
+        # Set headers to explicitly disable caching
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, max-age=0"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
+        return response
+
 app.mount(
     path="/",
-    app=StaticFiles(
+    app=NoCacheStaticFiles(
         directory="./src/static/",
         html=True,
     ),
